4ebb9e9d2c6835c5a9502299314007c1c1b311bd
Fagner <fagnerrc12@gmail.com>
2022-08-14 12:50:15 -0300

    Add images

--- dist/script.js
+++ dist/script.js
@@ -1,15 +1,31 @@
 var initial = null;
-var startingPoint = 30;
+var startingPoint = 0;
+var radius = 200;
+var blockSize = 50;
 var animationTimers = [];
 var elementNum = 5;
+var transitionTime = 1000;
+var transition = "all ".concat(transitionTime, "ms cubic-bezier(0.9, 2.1, 0.8, 1)");
 function start() {
+    var imgs = [
+        "gradient.png",
+        "layers.png",
+        "pen-tool.png",
+        "performance.png",
+        "web-design.png",
+    ];
     for (var i = 0; i < elementNum; i++) {
         var color = (360 / elementNum) * i;
         var angle = startingPoint + color;
+        var img = document.createElement("img");
+        img.width = 50;
+        img.height = 50;
+        img.src = imgs[i];
         var block = document.createElement("div");
         block.className = "block";
         block.style.transform = "rotate(".concat(-angle, "deg)");
-        block.style.backgroundColor = "hsl(".concat(color, ", 80%, 60%)");
+        // block.style.backgroundColor = `hsl(${color}, 80%, 60%)`;
+        block.appendChild(img);
         var rotate = document.createElement("div");
         rotate.className = "rotate-box";
         rotate.id = "r".concat(i);
@@ -18,7 +34,7 @@ function start() {
         var half = Math.floor(elementNum / 2) + 1;
         var blur_1 = i >= half ? elementNum - i : i;
         blur_1 %= half;
-        blur_1 *= 5;
+        blur_1 *= 2;
         block.style.filter = "blur(".concat(blur_1, "px)");
         document.querySelector(".box").appendChild(rotate);
         setupEventListeners(rotate);
@@ -45,13 +61,13 @@ function setupEventListeners(rotate) {
             oldAngle = parseFloat(element.style.transform.replace("rotate(", ""));
             var angle = oldAngle + direction;
             var child = element.firstElementChild;
-            element.style.transition = "all 2s ease-in";
-            child.style.transition = "all 2s ease-in";
+            element.style.transition = transition;
+            child.style.transition = transition;
             element.style.transform = "rotate(".concat(angle, "deg)");
             child.style.transform = "rotate(".concat(-angle, "deg)");
             var blur_2 = pos >= half ? elementNum - pos : pos;
             blur_2 %= half;
-            blur_2 *= 5;
+            blur_2 *= 2;
             child.style.filter = "blur(".concat(blur_2, "px)");
             var timeout = setTimeout(function (funcElement, funcChild, funcAngle) {
                 funcElement.style.transition = "any";
@@ -59,14 +75,14 @@ function setupEventListeners(rotate) {
                 funcAngle = funcAngle < 0 ? 360 + funcAngle : funcAngle;
                 funcElement.style.transform = "rotate(".concat(funcAngle % 360, "deg)");
                 funcChild.style.transform = "rotate(".concat(-(funcAngle % 360), "deg)");
-            }.bind(this, element, child, angle), 2000);
+            }.bind(this, element, child, angle), transitionTime);
             animationTimers.push(timeout);
             pos++;
             i = (elementNum + i - 1) % elementNum;
             element = document.querySelector("#r".concat(i));
         } while (i != index);
         initial = rotate;
-    }.bind(this, 8));
+    });
 }
 start();
 //# sourceMappingURL=script.js.map
--- src/script.ts
+++ src/script.ts
@@ -1,18 +1,33 @@
 let initial: HTMLElement = null;
-let startingPoint = 30;
+let startingPoint = 0;
+let radius = 200;
+let blockSize = 50;
 let animationTimers: number[] = [];
 const elementNum = 5;
+const transitionTime = 1000;
+const transition = `all ${transitionTime}ms cubic-bezier(0.9, 2.1, 0.8, 1)`;
 
 function start() {
+  let imgs = [
+    "gradient.png",
+    "layers.png",
+    "pen-tool.png",
+    "performance.png",
+    "web-design.png",
+  ];
   for (var i = 0; i < elementNum; i++) {
     let color = (360 / elementNum) * i;
     let angle = startingPoint + color;
+    let img = document.createElement("img");
+    img.width = 50;
+    img.height = 50;
+    img.src = imgs[i];
 
     var block = document.createElement("div");
     block.className = "block";
     block.style.transform = `rotate(${-angle}deg)`;
-    block.style.backgroundColor = `hsl(${color}, 80%, 60%)`;
-
+    // block.style.backgroundColor = `hsl(${color}, 80%, 60%)`;
+    block.appendChild(img);
     var rotate = document.createElement("div");
     rotate.className = "rotate-box";
     rotate.id = `r${i}`;
@@ -22,7 +37,7 @@ function start() {
     let half = Math.floor(elementNum / 2) + 1;
     let blur = i >= half ? elementNum - i : i;
     blur %= half;
-    blur *= 5;
+    blur *= 2;
     block.style.filter = `blur(${blur}px)`;
 
     (<HTMLElement>document.querySelector(".box")).appendChild(rotate);
@@ -31,69 +46,66 @@ function start() {
 }
 
 function setupEventListeners(rotate: HTMLElement) {
-  rotate.addEventListener(
-    "click",
-    function () {
-      if (animationTimers.length > 0) {
-        animationTimers.forEach((x) => window.clearTimeout(x));
-        animationTimers = [];
-      }
+  rotate.addEventListener("click", function () {
+    if (animationTimers.length > 0) {
+      animationTimers.forEach((x) => window.clearTimeout(x));
+      animationTimers = [];
+    }
 
-      let index: number = parseInt(rotate.id.replace("r", ""));
-      let i = index;
-      let element = rotate;
-      initial ??= document.querySelector("#r0");
-      let oldAngle = parseFloat(element.style.transform.replace("rotate(", ""));
-      let targetAngle = parseFloat(
-        initial.style.transform.replace("rotate(", "")
+    let index: number = parseInt(rotate.id.replace("r", ""));
+    let i = index;
+    let element = rotate;
+    initial ??= document.querySelector("#r0");
+    let oldAngle = parseFloat(element.style.transform.replace("rotate(", ""));
+    let targetAngle = parseFloat(
+      initial.style.transform.replace("rotate(", "")
+    );
+    let left = -(Math.abs(oldAngle) - targetAngle);
+    let right = 360 - (Math.abs(oldAngle) - targetAngle);
+    let direction = Math.abs(left) < Math.abs(right) ? left : right;
+    let pos = 0;
+    let half = Math.floor(elementNum / 2) + 1;
+
+    do {
+      oldAngle = parseFloat(element.style.transform.replace("rotate(", ""));
+      let angle = oldAngle + direction;
+
+      let child = <HTMLElement>element.firstElementChild;
+
+      element.style.transition = transition;
+      child.style.transition = transition;
+
+      element.style.transform = `rotate(${angle}deg)`;
+      child.style.transform = `rotate(${-angle}deg)`;
+
+      let blur = pos >= half ? elementNum - pos : pos;
+      blur %= half;
+      blur *= 2;
+      child.style.filter = `blur(${blur}px)`;
+
+      let timeout = setTimeout(
+        function (
+          funcElement: HTMLElement,
+          funcChild: HTMLElement,
+          funcAngle: number
+        ) {
+          funcElement.style.transition = "any";
+          funcChild.style.transition = "any";
+
+          funcAngle = funcAngle < 0 ? 360 + funcAngle : funcAngle;
+          funcElement.style.transform = `rotate(${funcAngle % 360}deg)`;
+          funcChild.style.transform = `rotate(${-(funcAngle % 360)}deg)`;
+        }.bind(this, element, child, angle),
+        transitionTime
       );
-      let left = -(Math.abs(oldAngle) - targetAngle);
-      let right = 360 - (Math.abs(oldAngle) - targetAngle);
-      let direction = Math.abs(left) < Math.abs(right) ? left : right;
-      let pos = 0;
-      let half = Math.floor(elementNum / 2) + 1;
+      animationTimers.push(timeout);
+      pos++;
+      i = (elementNum + i - 1) % elementNum;
+      element = document.querySelector(`#r${i}`);
+    } while (i != index);
 
-      do {
-        oldAngle = parseFloat(element.style.transform.replace("rotate(", ""));
-        let angle = oldAngle + direction;
-
-        let child = <HTMLElement>element.firstElementChild;
-
-        element.style.transition = "all 2s ease-in";
-        child.style.transition = "all 2s ease-in";
-
-        element.style.transform = `rotate(${angle}deg)`;
-        child.style.transform = `rotate(${-angle}deg)`;
-
-        let blur = pos >= half ? elementNum - pos : pos;
-        blur %= half;
-        blur *= 5;
-        child.style.filter = `blur(${blur}px)`;
-
-        let timeout = setTimeout(
-          function (
-            funcElement: HTMLElement,
-            funcChild: HTMLElement,
-            funcAngle: number
-          ) {
-            funcElement.style.transition = "any";
-            funcChild.style.transition = "any";
-
-            funcAngle = funcAngle < 0 ? 360 + funcAngle : funcAngle;
-            funcElement.style.transform = `rotate(${funcAngle % 360}deg)`;
-            funcChild.style.transform = `rotate(${-(funcAngle % 360)}deg)`;
-          }.bind(this, element, child, angle),
-          2000
-        );
-        animationTimers.push(timeout);
-        pos++;
-        i = (elementNum + i - 1) % elementNum;
-        element = document.querySelector(`#r${i}`);
-      } while (i != index);
-
-      initial = rotate;
-    }.bind(this, 8)
-  );
+    initial = rotate;
+  });
 }
 
 start();
